33d359cdfc454d3524580a1458a27db022f51cfb
Mikhail <mspnovator@gmail.com>
2025-04-25 21:23:13 +0300

    Updated home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import styles from './Home.module.css';
 const Home = () => {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Добро пожаловать на Tutor!</h1>
+            <h1 className={styles.title}>Добро пожаловать на Tutor Assistant!</h1>
             <p className={styles.subtitle}>
                 Ваш путь к знаниям начинается здесь. Найдите лучших учителей и откройте новые горизонты обучения.
             </p>
